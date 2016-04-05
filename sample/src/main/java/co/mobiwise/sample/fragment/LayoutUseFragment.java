7f8316aac7b95b532f3b60bea2da417a567f612c
YamLee <liyaninhn@gmail.com>
2016-04-05 17:05:26 +0800

    Demonstration for use custom info view

--- sample/src/main/java/co/mobiwise/sample/fragment/LayoutUseFragment.java
+++ sample/src/main/java/co/mobiwise/sample/fragment/LayoutUseFragment.java
@@ -1,5 +1,6 @@
 package co.mobiwise.sample.fragment;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import co.mobiwise.materialintro.InfoViewConfiguration;
 import co.mobiwise.materialintro.shape.Focus;
 import co.mobiwise.materialintro.shape.FocusGravity;
 import co.mobiwise.materialintro.view.MaterialIntroView;
@@ -28,7 +30,18 @@ public class LayoutUseFragment extends Fragment {
         Button button = (Button) view.findViewById(R.id.btn_text);
         MaterialIntroView materialIntroView = (MaterialIntroView) inflater.inflate(R.layout.layout_custom_material_intro_view, null);
         MaterialIntroView.Builder builder = new MaterialIntroView.Builder(materialIntroView);
-        mMaterialIntroView = builder.enableDotAnimation(true)
+        View customInfoView = inflater.inflate(R.layout.layout_custom_info, null);
+        InfoViewConfiguration infoViewConfiguration = new InfoViewConfiguration();
+        infoViewConfiguration.setInfoView(customInfoView);
+        View ivArrow = customInfoView.findViewById(R.id.iv_arrow);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(ivArrow, View.TRANSLATION_Y, 0, 20, 0);
+        objectAnimator.setDuration(1000);
+        objectAnimator.setRepeatCount(ObjectAnimator.INFINITE);
+        objectAnimator.setRepeatMode(ObjectAnimator.REVERSE);
+        infoViewConfiguration.setAnimator(objectAnimator);
+        infoViewConfiguration.setAlignCenter(true);
+        mMaterialIntroView = builder
+                .enableDotAnimation(false)
                 .enableIcon(false)
                 .setFocusGravity(FocusGravity.CENTER)
                 .setFocusType(Focus.MINIMUM)
@@ -36,6 +49,7 @@ public class LayoutUseFragment extends Fragment {
                 .enableFadeAnimation(true)
                 .performClick(true)
                 .setInfoText("Hi There! Click this card and see what happens.")
+                .setInfoViewConfiguration(infoViewConfiguration)
                 .setTarget(button)
                 .setUsageId("intro_card") //THIS SHOULD BE UNIQUE ID
                 .build();
